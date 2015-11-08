621c0cb07aa1df11d3968c2fd0743bfc034cff7e
Davidson Zheng <nadavidsonzheng@gmail.com>
2015-11-07 23:57:50 -0500

    Changed to 3 photocells plus control 3 servo motors with improved timing control; need to optimize for deceleration

--- mirrormotion/mirrormotion.ino
+++ mirrormotion/mirrormotion.ino
@@ -8,20 +8,21 @@ For more information see http://learn.adafruit.com/photocells
 
 Wooden Mirror sketch created on Novemeber 7th, 2015
 
-Iteration No.1 control 4 photoresistors + 4 servo motors (180 degree)
-servo arrangement (facing the front of the frames) 
-1 -> top left
-2 -> top right
-3 -> bottom left
-4 -> bottom right
+Iteration No.2 control 3 photoresistors + 3 servo motors (180 degree)
+servo arrangement in a column
+1 -> top
+2 -> middle
+3 -> bottom
 */
-#define NUM_PR 4 // numbers of photoresistors
-#define PR_THRESHOLD 180
+
+#define DEBUG 0
+
+#define NUM_PR 3 // numbers of photoresistors
+#define PR_THRESHOLD 900
 
 #define SERVO_PIN_1 8
 #define SERVO_PIN_2 9
 #define SERVO_PIN_3 10
-#define SERVO_PIN_4 11
 
 /* push distance is rated by the proximity between the person and the frame 
 0 -> initial position,
@@ -34,90 +35,171 @@ servo arrangement (facing the front of the frames)
 #define PUSH_DIS_2 120
 #define PUSH_DIS_3 180
 
-int PR_VAL_1;
-int PR_VAL_2;
-int PR_VAL_3;
-int PR_VAL_4;
+//int frame1PushedOut = 0;
+//int frame2PushedOut = 0;
+//int frame3PushedOut = 0;
 
-int frame1PushedOut = 0;
-int frame2PushedOut = 0;
-int frame3PushedOut = 0;
-int frame4PushedOut = 0;
+int lastServoPos1 = INIT_POS;
+int lastServoPos2 = INIT_POS;
+int lastServoPos3 = INIT_POS;
 
 Servo servo1;
 Servo servo2;
 Servo servo3;
-Servo servo4;
+
+/* better timing control */
+int  updateInterval;      // interval between updates
+unsigned long lastUpdate; // last update of position
+
+class Slider
+{
+  Servo servo;              // the servo
+  int pos;              // current servo position
+  int lastPos;
+  int  updateInterval;      // interval between updates
+  unsigned long lastUpdate; // last update of position
+ 
+public: 
+  Slider(int interval)
+  {
+    updateInterval = interval;
+  }
+  
+  void Attach(int pin)
+  {
+    servo.attach(pin);
+  }
+  
+  void Update()
+  {
+    if((millis() - lastUpdate) > updateInterval)  // time to update
+    {
+      lastUpdate = millis();
+      servo.write(pos);
+//      Serial.println(pos);
+    }
+  }
+};
 
 void setup(void) {
+  Serial.begin(9600);
+  
   servo1.attach(SERVO_PIN_1);
   servo2.attach(SERVO_PIN_2);
   servo3.attach(SERVO_PIN_3);
-  servo4.attach(SERVO_PIN_4);
   
   servo1.write(INIT_POS);
   servo2.write(INIT_POS);
   servo3.write(INIT_POS);
-  servo4.write(INIT_POS);
-  
-  Serial.begin(9600);   
 }
- 
-void loop(void) {
-  int prVal = 0;
-  
-  for (int i=0; i<4; i++) {
-    prVal = analogRead(i);
-    
-    Serial.print("Analog reading = ");
-    Serial.println(prVal);
 
-    if (prVal >= PR_THRESHOLD) {
-      if (!pushedOut) {
-        pushedOut = 1;
-        digitalWrite(LEDpin, HIGH);
-        goIn();
-      }
-    } else {
-      if (pushedOut) {
-        pushedOut = 0;
-        digitalWrite(LEDpin, LOW);
-        goOut();
-      }
+void loop(void) {
+  if (DEBUG) {
+    int prVal = 0;
+    for (int j=0; j<NUM_PR; j++) {
+      prVal = analogRead(j);
+      Serial.print("Sensor ");
+      Serial.print(j);
+      Serial.print("is ");
+      Serial.println(prVal);
     }
-  
-    switch i {
-      case 0:
-        break;
-      case 1:
-        break;
-      case 2:
-        break;
-      case 3:
-        break;
-      default:
-        break;
+//    delay(10);
+  } else {
+    int prVal = 0;
+    for (int i=0; i<NUM_PR; i++) {
+      prVal = analogRead(i);
+      
+//      Serial.print("Sensor ");
+//      Serial.print(i);
+//      Serial.print("is ");
+//      Serial.println(prVal);
+      
+      square_motion(i, prVal);
     }
   }
- 
-  // LED gets brighter the darker it is at the sensor
-  // that means we have to -invert- the reading from 0-1023 back to 1023-0
-//  photocellReading = 1023 - photocellReading;
-  //now we have to map 0-1023 to 0-255 since thats the range analogWrite uses
-//  LEDbrightness = map(photocellReading, 0, 1023, 0, 255);
-  
-  
   delay(100);
 }
 
-void goIn() {
-  servo1.write(180);
+void square_motion(int pr, int prVal) {
+  int servoPos = compute_distance(prVal);
+  switch (pr) {
+    case 0:
+      if (servoPos > lastServoPos1) {
+        servo1.write(servoPos);
+      } else {
+        int updateInterval = 15;
+        unsigned long lastUpdate;
+        Serial.print("current time is ");
+        Serial.println(millis());
+        
+        // if last servo position is further, slowly move back
+        for (int i = lastServoPos1; i >= servoPos; i --) {
+          if ((millis() - lastUpdate) > updateInterval) {
+            lastUpdate = millis();
+            Serial.print("last time is ");
+            Serial.println(lastUpdate);
+            if (i <= (lastServoPos1 - servoPos)*0.2) {
+              updateInterval *= 0.8;
+            }
+            servo1.write(i);
+          }
+        }
+      }
+      
+      lastServoPos1 = servoPos;
+      break;
+    case 1:
+      if (servoPos > lastServoPos2) {
+        servo2.write(servoPos);
+      } else {
+        int updateInterval = 10;
+        unsigned long lastUpdate;
+        // if last servo position is further, slowly move back
+        for (int i = lastServoPos2; i >= servoPos; i --) {
+          if ((millis() - lastUpdate) > updateInterval) {
+            lastUpdate = millis();
+            if (i <= (lastServoPos2 - servoPos)*0.2) {
+              updateInterval *= 0.8;
+            }
+            servo2.write(i);
+          }
+        }
+      }
+      
+      lastServoPos2 = servoPos;
+      break;
+    case 2:
+      if (servoPos > lastServoPos3) {
+        servo3.write(servoPos);
+      } else {
+        int updateInterval = 15;
+        unsigned long lastUpdate;
+        // if last servo position is further, slowly move back
+        for (int i = lastServoPos3; i >= servoPos; i --) {
+          if ((millis() - lastUpdate) > updateInterval) {
+            lastUpdate = millis();
+            if (i <= (lastServoPos3 - servoPos)*0.2) {
+              updateInterval *= 0.8;
+            }
+            servo3.write(i);
+          }
+        }
+      }
+      
+      lastServoPos3 = servoPos;
+      break;
+    default:
+      break;
+  }
 }
 
-void goOut() {
-  for (int i = 180; i >= 0; i --) {
-       servo1.write(i);
-       delay(5);
+int compute_distance(int prVal) {
+  if (prVal > PR_THRESHOLD*0.8 && prVal <= PR_THRESHOLD) {
+    return INIT_POS;
+  } else if (prVal > PR_THRESHOLD*0.6 && prVal <= PR_THRESHOLD*0.8) {
+    return PUSH_DIS_1;
+  } else {
+    return PUSH_DIS_3;
   }
 }
 
