b6d81d7d19d42de4198eb6599dc4554d3d2d2303
Davidson Zheng <nadavidsonzheng@gmail.com>
2015-11-09 00:44:11 -0500

    Deleted unused code; changed pin mapping for servo motors; changed motor value computation logic

--- mirrormotion/mirrormotion.ino
+++ mirrormotion/mirrormotion.ino
@@ -5,9 +5,7 @@ Connect one end of the photocell to 5V, the other end to Analog 0.
 Then connect one end of a 10K resistor from Analog 0 to ground 
 Connect LED from pin 11 through a resistor to ground 
 For more information see http://learn.adafruit.com/photocells 
-
-Wooden Mirror sketch created on Novemeber 7th, 2015
-
+Wooden Mirror sketch created on Novemeber 9th, 2015
 Iteration No.2 control 3 photoresistors + 3 servo motors (180 degree)
 servo arrangement in a column
 1 -> top
@@ -18,11 +16,11 @@ servo arrangement in a column
 #define DEBUG 0
 
 #define NUM_PR 3 // numbers of photoresistors
-#define PR_THRESHOLD 900
+#define PR_THRESHOLD 800
 
-#define SERVO_PIN_1 8
-#define SERVO_PIN_2 9
-#define SERVO_PIN_3 10
+#define SERVO_PIN_1 5
+#define SERVO_PIN_2 4
+#define SERVO_PIN_3 3
 
 /* push distance is rated by the proximity between the person and the frame 
 0 -> initial position,
@@ -35,10 +33,6 @@ servo arrangement in a column
 #define PUSH_DIS_2 120
 #define PUSH_DIS_3 180
 
-//int frame1PushedOut = 0;
-//int frame2PushedOut = 0;
-//int frame3PushedOut = 0;
-
 int lastServoPos1 = INIT_POS;
 int lastServoPos2 = INIT_POS;
 int lastServoPos3 = INIT_POS;
@@ -46,40 +40,6 @@ int lastServoPos3 = INIT_POS;
 Servo servo1;
 Servo servo2;
 Servo servo3;
-
-/* better timing control */
-int  updateInterval;      // interval between updates
-unsigned long lastUpdate; // last update of position
-
-class Slider
-{
-  Servo servo;              // the servo
-  int pos;              // current servo position
-  int lastPos;
-  int  updateInterval;      // interval between updates
-  unsigned long lastUpdate; // last update of position
- 
-public: 
-  Slider(int interval)
-  {
-    updateInterval = interval;
-  }
-  
-  void Attach(int pin)
-  {
-    servo.attach(pin);
-  }
-  
-  void Update()
-  {
-    if((millis() - lastUpdate) > updateInterval)  // time to update
-    {
-      lastUpdate = millis();
-      servo.write(pos);
-//      Serial.println(pos);
-    }
-  }
-};
 
 void setup(void) {
   Serial.begin(9600);
@@ -103,41 +63,40 @@ void loop(void) {
       Serial.print("is ");
       Serial.println(prVal);
     }
-//    delay(10);
   } else {
     int prVal = 0;
     for (int i=0; i<NUM_PR; i++) {
       prVal = analogRead(i);
       
-//      Serial.print("Sensor ");
-//      Serial.print(i);
-//      Serial.print("is ");
-//      Serial.println(prVal);
+      Serial.print("Sensor ");
+      Serial.print(i);
+      Serial.print("is ");
+      Serial.println(prVal);
       
       square_motion(i, prVal);
     }
   }
-  delay(100);
 }
 
+/* */
 void square_motion(int pr, int prVal) {
   int servoPos = compute_distance(prVal);
   switch (pr) {
-    case 0:
+    case 0: // servo motor 1
       if (servoPos > lastServoPos1) {
         servo1.write(servoPos);
       } else {
         int updateInterval = 15;
         unsigned long lastUpdate;
-        Serial.print("current time is ");
-        Serial.println(millis());
+//        Serial.print("current time is ");
+//        Serial.println(millis());
         
         // if last servo position is further, slowly move back
         for (int i = lastServoPos1; i >= servoPos; i --) {
           if ((millis() - lastUpdate) > updateInterval) {
             lastUpdate = millis();
-            Serial.print("last time is ");
-            Serial.println(lastUpdate);
+//            Serial.print("last time is ");
+//            Serial.println(lastUpdate);
             if (i <= (lastServoPos1 - servoPos)*0.2) {
               updateInterval *= 0.8;
             }
@@ -148,7 +107,7 @@ void square_motion(int pr, int prVal) {
       
       lastServoPos1 = servoPos;
       break;
-    case 1:
+    case 1: // servo motor 2
       if (servoPos > lastServoPos2) {
         servo2.write(servoPos);
       } else {
@@ -168,7 +127,7 @@ void square_motion(int pr, int prVal) {
       
       lastServoPos2 = servoPos;
       break;
-    case 2:
+    case 2: // servo motor 3
       if (servoPos > lastServoPos3) {
         servo3.write(servoPos);
       } else {
@@ -193,13 +152,11 @@ void square_motion(int pr, int prVal) {
   }
 }
 
+/* map photoresistor value to motor value, the brighter the greater the motor value gets */
 int compute_distance(int prVal) {
-  if (prVal > PR_THRESHOLD*0.8 && prVal <= PR_THRESHOLD) {
+  if (prVal <= PR_THRESHOLD) {
     return INIT_POS;
-  } else if (prVal > PR_THRESHOLD*0.6 && prVal <= PR_THRESHOLD*0.8) {
-    return PUSH_DIS_1;
   } else {
     return PUSH_DIS_3;
   }
 }
-
